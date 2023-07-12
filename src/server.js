34d51c95089d6089a943eadf6b96e5f72ee5b274
cslee <cslee0034@gmail.com>
2023-07-12 23:39:48 +0900

    feat: add try catch

--- src/server.js
+++ src/server.js
@@ -12,20 +12,30 @@ const server = async () => {
   try {
     await mongoose.connect(MONGO_URI);
     console.log("MongoDB connected");
-
     app.use(express.json());
 
-    app.get("./user", (req, res) => {
-      // return res.send({ users: users })
-    });
-
     app.post("/user", async (req, res) => {
-      const user = new User({ username: req.body });
-      await user.save();
-      return res.sned({ user });
+      try {
+        let { username, name } = req.body;
+
+        if (!username)
+          return res.status(400).send({ err: "username is required" });
+
+        if (!name || !name.first || !name.last)
+          return res
+            .status(400)
+            .send({ err: "Both first and last names are requied" });
+
+        const user = new User(username, name);
+        await user.save();
+        return res.send({ user });
+      } catch (error) {
+        console.log(error);
+        return res.status(500).send({ err: error.message });
+      }
     });
 
-    app.listen(3000, function () {
+    app.listen(3000, () => {
       console.log("server listning on port 3000");
     });
   } catch (error) {
