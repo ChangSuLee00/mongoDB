c73dfac371ad539f9c43576fb09424f273c8a736
cslee0034 <cslee0034@gmail.com>
2023-12-16 13:45:21 +0900

    feat: add user create logic

--- components/users/service.js
+++ components/users/service.js
@@ -1,11 +1,29 @@
-class Service {
-  getUser = async (email) => {
-    return User.findOne(email);
-  };
+import User from "../../models/User.js";
+import Region from "../../models/Region.js";
+import createError from "http-errors";
 
-  createUser = async ({ email, address, addressDetail }) => {
-    return "hello";
-  };
+class Service {
+  async createUser({ email, country, city, district, addressDetail }) {
+    if (!email || !country || !city || !district) {
+      throw createError(400, "Missing required fields");
+    }
+
+    let region = await Region.findOne({ country, city, district });
+    if (!region) {
+      region = new Region({ country, city, district });
+      await region.save();
+    }
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      throw createError(409, "User already exists with this email");
+    }
+
+    const newUser = new User({ email, address: region._id, addressDetail });
+    await newUser.save();
+
+    return newUser;
+  }
 }
 
 export default Service;
